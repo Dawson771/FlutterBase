c00a59a1a71af838cc7ff120552a775c7d49a60f
Dawson771 <15264796+dawson771@user.noreply.gitee.com>
2026-03-17 19:09:50 +0800

    主页tab栏的实现

--- hm_shop/lib/pages/Main/index.dart
+++ hm_shop/lib/pages/Main/index.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:hm_shop/pages/Cart/index.dart';
+import 'package:hm_shop/pages/Category/index.dart';
+import 'package:hm_shop/pages/Home/index.dart';
+import 'package:hm_shop/pages/Mine/index.dart';
 
 class MainPage extends StatefulWidget {
   MainPage({Key? key}) : super(key: key);
@@ -8,11 +12,74 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  //定义数据根据数据进行渲染4个导航
+  // 一般应用程序的导航是固定的
+  List<Map<String, String>> _tabList = [
+    {
+      "icon": "lib/assets/home1.png", //正常显示图标
+      "icon_active": "lib/assets/home2.png", //选中显示图标
+      "text": "首页",
+    },
+    {
+      "icon": "lib/assets/mingcute_classify-line.png", //正常显示图标
+      "icon_active": "lib/assets/mingcute_classify-fill.png", //选中显示图标
+      "text": "分类",
+    },
+    {
+      "icon": "lib/assets/icon-park-outline_shopping.png", //正常显示图标
+      "icon_active": "lib/assets/icon-park-solid_shopping.png", //选中显示图标
+      "text": "购物车",
+    },
+    {
+      "icon": "lib/assets/tdesign_setting.png", //正常显示图标
+      "icon_active": "lib/assets/tdesign_setting-filled.png", //选中显示图标
+      "text": "我的",
+    },
+  ];
+  // 返回获取底部导航栏
+  List<BottomNavigationBarItem> _getTabBarWidget() {
+    return List.generate(_tabList.length, (int index) {
+      return BottomNavigationBarItem(
+        icon: Image.asset(_tabList[index]["icon"]!, width: 30, height: 30),
+        activeIcon: Image.asset(
+          _tabList[index]["icon_active"]!,
+          width: 30,
+          height: 30,
+        ),
+        label: _tabList[index]["text"],
+      );
+    });
+  }
+
+  List<Widget> _getChildren() {
+    return [HomeView(), CategoryView(), CartView(), MineView()];
+  }
+
+  int _currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("主页")),
-      body: Center(child: Text("主页")),
+      //SafeArea 避开安全区域组件
+      body: SafeArea(
+        child: IndexedStack(
+          index: _currentIndex,
+          children: _getChildren(), // 子组件多个时使用IndexedStack
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.black, // 选中颜色
+        showUnselectedLabels: true, // 显示未选中的标签
+        unselectedItemColor: Colors.grey, // 未选中颜色
+        onTap: (int index) {
+          print(index);
+          // 设置点击的索引
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        currentIndex: _currentIndex,
+        items: _getTabBarWidget(),
+      ),
     );
   }
 }
