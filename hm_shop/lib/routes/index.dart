f14fd733a65dfa368b426f73d45e1c78e7f8ecdc
Dawson771 <15264796+dawson771@user.noreply.gitee.com>
2026-03-17 15:13:52 +0800

    搭建基础路由和组件

---
+++ hm_shop/lib/routes/index.dart
@@ -0,0 +1,20 @@
+//管理路由
+import 'package:flutter/material.dart';
+import 'package:hm_shop/pages/Login/index.dart';
+import 'package:hm_shop/pages/Main/index.dart';
+
+Widget getRootWidget() {
+  return MaterialApp(
+    //命名路由
+    initialRoute: '/',
+    routes: getRootRoutes(),
+  );
+}
+
+//返回该app的路由的配置
+Map<String, Widget Function(BuildContext)> getRootRoutes() {
+  return {
+    '/': (context) => MainPage(), //主页
+    '/login': (context) => LoginPage(), //登录页面
+  };
+}
